59f9c02898365c03f51bd23337df8c76b5b11b49
Emmanuel Semsella <emma.semsella06@gmail.com>
2026-01-31 16:18:24 +0100

    'done'

--- problem1.py
+++ problem1.py
@@ -1,5 +1,30 @@
-import sympy as sp
+# Consider the system of equations
 
-### Replace with your own code (begin) ###
+# \( 2x^2 + 3y^2 = r \) and \( y = 2x + 1 \)
+#where \( r \) is a real parameter
+
+###your task;
+# Write your code  using **SymPy** that:
+
+# -defines the symbolic variables `x`, `y`, and `r`,
+# - symbolically solve the system for `x` and `y`,
+# - stores **all solutions** in a list named `sol`,
+# - where each element of `sol` is a dictionary mapping
+#  `{x: ..., y: ...}` in terms of the parameter `r`.
+
+#Your solution must be **fully symbolic** and must not substitute a numerical value for `r` in this problem.
+
+
+from sympy import symbols, Eq, solve
+
+x, y, r = symbols('x, y, r', real=True)
+
+eq1 = Eq(2*x**2 + 3*y**2, r)
+eq2 = Eq(y - 2*x, 1)
+
+sol = solve([eq1, eq2], [x, y], dict=True)
+
+print(sol)
+
 pass
-### Replace with your own code (end) ###
+
--- problem2.py
+++ problem2.py
@@ -1,7 +1,41 @@
 import numpy as np
-import sympy as sp
 import matplotlib.pyplot as plt
+from sympy import symbols, Eq, solve, sqrt, lambdify
 
-### Replace with your own code (begin) ###
-pass
-### Replace with your own code (end) ###
+x, y, r = symbols('x, y, r', real=True)
+
+eq1 = Eq(2*x**2 + 3*y**2, r)
+eq2 = Eq(y - 2*x, 1)
+
+sol = solve([eq1, eq2], [x, y], dict=True)
+
+r_val = 10
+
+points = [(float(s[x].subs(r, r_val)), 
+           float(s[y].subs(r, r_val))) for s in sol]
+
+y_line = lambdify(x, 2*x + 1, 'numpy')
+
+y_ellipse = lambdify(x, sqrt((r_val - 2*x**2)/3), 'numpy')
+
+x_vals = np.linspace(-np.sqrt(5), np.sqrt(5), 400)
+
+plt.plot(x_vals, y_line(x_vals), label='y=2x+1')
+
+plt.plot(x_vals, y_ellipse(x_vals), label='2x^2 + 3y^2 = 10')
+plt.plot(x_vals, -y_ellipse(x_vals))
+
+for px, py in points:
+    plt.plot(float(px), float(py),
+             marker='*', markersize='10',
+             markeredgecolor='black', markerfacecolor='red')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Ellipse and Line')
+plt.grid(True)
+plt.legend()
+plt.savefig('Problem2.pdf')
+plt.show()
+
+
